f16e337c5f009c9780460db2fdb954d754a8fbd5
sangareswari <sangareswari@syngrid.com>
2025-12-29 12:29:23 +0530

    initial

---
+++ modules/Footer.module/module.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+  "use strict";
+  if( $('.container-fullwidth').length ){
+    $('.container-fullwidth').parents('.dnd-section').addClass('dnd-section-fullwidth');
+  }  
+  
+});  
